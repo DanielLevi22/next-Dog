fcf67eed75640b7df6f86eaa4f167bf5bd0f5888
Daniel Levi <daniellevi000@gmail.com>
2024-03-03 14:30:04 -0300

    pegando o cookies pelo token para verificacao de usuario logado

---
+++ src/actions/getuser.ts
@@ -0,0 +1,41 @@
+'use server'
+
+import { USER_GET } from "@/utils/api";
+import { apiError } from "@/utils/apierror";
+import { cookies } from "next/headers";
+
+
+export type User = {
+  id: number;
+  email: string;
+  username: string;
+  nome: string;
+}
+export default async function getUser() {
+  try {
+    const token = cookies().get('token')?.value
+    if(!token) throw new Error("Token nao encontrado")
+    const { url } = USER_GET()
+    
+    const response = await fetch( url, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+      next: {
+        revalidate: 60
+      }
+    })
+    
+    if(!response.ok) throw new Error( 'Error ao pegar usuario.')
+    
+    const data = await response.json() as User
+
+    return { data, ok: true,  error: ''}
+    
+  } catch (error:unknown) {
+    return apiError(error)
+  }
+
+
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 import styles from './Header.module.css'
 import Image from "next/image";
-export function Header() {
-  const user = true
+import getUser from "@/actions/getuser";
+export async function Header() {
+  const { data } = await getUser()
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link href="/" className={styles.logo}>
           <Image src="/assets/dogs.svg" alt="dogs"  width={28} height={22} priority />
         </Link>
-        {user ? (
-        <Link href="/conta" className={styles.login} >Dogs</Link>
+        {data ? (
+        <Link href="/conta" className={styles.login} >{data.username}</Link>
         ): (
         <Link href="/login" className={styles.login} >Login / Criar</Link>
         )}
