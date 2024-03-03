bbbd532bfe45c19cd29b70fc7f5c905165527c63
Daniel Levi <daniellevi000@gmail.com>
2024-03-03 10:16:40 -0300

    adicionando form status botao de form pagina login

--- src/components/login/loginform.tsx
+++ src/components/login/loginform.tsx
@@ -1,13 +1,32 @@
+'use client'
 import login from "@/actions/login";
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/forms/button";
+
+
+function FormButton() {
+  const  { pending } = useFormStatus()
+  return (
+    <>
+    { pending ? (
+      <Button disabled={pending}>Enviando...</Button>
+    ):(
+      <Button >Entrar</Button>
+    )}
+    </>
+  )
+}
 
 
 export function LoginForm() {
+
+
   return(
     <>
       <form action={login}>
         <input type="text" placeholder="usuario" name="username"/>
         <input type="password" placeholder="senha" name="password"/>
-        <button>Entrar</button>
+        <FormButton />
       </form>
     </>
   )
